5da95c41f93cddda83f56fbb8ccf34367c0f26cd
yamadapc <tacla.yamada@gmail.com>
2015-05-03 15:56:01 -0300

    Fix the first test case and start serious dev.

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,30 @@
-'use strict'; /* global describe, it */
+'use strict'; /* global describe, it, before */
 var fs = require('fs');
+var path = require('path');
+var remove = require('remove');
 var should = require('should');
 var requireGit = require('..');
 
 describe('require-git', function() {
   describe('requireGit', function() {
+    before(function(done) {
+      var t = path.join(__dirname, '..', 'node_modules', 'written-number');
+      fs.exists(t, function(e) {
+        if(e) {
+          return remove(t, done);
+        }
+        done();
+      });
+    });
+
+    it('gets exposed', function() {
+      should.exist(requireGit);
+      requireGit.should.be.instanceof(Function);
+    });
+
     it('installs from a git repository and returns its module', function() {
+      var old = fs.readdirSync(path.join(__dirname, '..', 'node_modules'));
+      old.should.not.include('written-number');
     });
   });
 });
