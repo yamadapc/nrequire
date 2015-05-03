400a2e1a20262b41e9165467beb88e90e9c7fb42
yamadapc <tacla.yamada@gmail.com>
2015-05-03 15:44:31 -0300

    Initial commit

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+'use strict'; /* global describe, it */
+var fs = require('fs');
+var should = require('should');
+var requireGit = require('..');
+
+describe('require-git', function() {
+  describe('requireGit', function() {
+    it('installs from a git repository and returns its module', function() {
+    });
+  });
+});
